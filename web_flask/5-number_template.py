108795d0caa02be0820621f2618d9aa81b8e591c
fadhuweb <f.abdulazee@alustudent.com>
2024-10-11 11:43:55 +0000

    task

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -7,12 +7,15 @@ It defines four routes:
 - Route for "/hbnb" that displays "HBNB"
 - Route for "/c/<text>" that displays "C <text>" where <text> can be any string
 - Route for "/python/(<text>)" that displays "Python <text>"
+- Route for the "/number/<n>" URL.
+- Route for the "/number_template/<n>" URL.
 
 Requirements:
 - Your web application must be listening on 0.0.0.0, port 5000
 """
 
-from flask import Flask
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -25,21 +28,33 @@ def index():
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Route for "/hbnb"."""
+    """Route for the /hbnb URL."""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """Route for "/c/<text>"."""
+    """Route for the /c/<text> URL."""
     return "C %s" % text.replace('_', ' ')
 
 
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-@app.route("/python/", strict_slashes=False)
-def python(text="is cool"):
-    """Route for "/python/<text>"."""
+def python(text):
+    """Route for the /python and /python/<text> URLs."""
     return "Python %s" % text.replace('_', ' ')
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Route for the /number/<n> URL. """
+    return "%d is a number" % n
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Route for the /number_template/<n> URL."""
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
